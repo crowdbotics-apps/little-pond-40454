f76e788a2324b6e6189817f61f118b8f7b3ab737
Crowdbotics <team@crowdbotics.com>
2023-05-04 17:33:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/little_pond_40454/MainActivity.java
@@ -0,0 +1,15 @@
+package com.little_pond_40454;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "little_pond_40454";
+  }
+}
